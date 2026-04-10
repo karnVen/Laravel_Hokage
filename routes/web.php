a1be883453fa1e4a0bbf05cf822fccad0f0b5a58
karnVen <onemonth06@gmail.com>
2026-04-10 07:49:59 +0000

    feat:Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+// routes/web.php
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Homepage - show all job listings
+Route::get('/jobs', [JobController::class, 'index'])->name('jobs.index');
+
+// Show a single job listing (e.g., /jobs/42)
+Route::get('/jobs/{id}', [JobController::class, 'show'])->name('jobs.show');
+
+// Show the "Post a Job" form
+Route::get('/jobs/create', [JobController::class, 'create'])->name('jobs.create');
+
+// Handle the form submission
+Route::post('/jobs', [JobController::class, 'store'])->name('jobs.store');
+
+// Delete a job listing
+Route::delete('/jobs/{id}', [JobController::class, 'destroy'])->name('jobs.destroy');
+
+// Redirect old URL to new URL
+Route::redirect('/listings', '/jobs');
